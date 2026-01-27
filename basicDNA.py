e1d9c9d60a7167fa89f82674bde29a9f5dc87677
PaulJMeyer <pauljonmeyer@web.de>
2026-01-27 15:48:24 +0100

    Version 2
    Updated validation from counting to using sets

--- basicDNA.py
+++ basicDNA.py
@@ -1,19 +1,16 @@
 seq = "ATGCAGTAGCACACAGTACAGTGTGACT"
 seq = seq.upper().replace("\n", "").replace(" ", "")
 
+allowed = {"A", "C", "G", "T"}
+
 print("Sequenz:", seq)
 
 while True:
 
-    length = len(seq)
-    A = seq.count("A")
-    C = seq.count("C")
-    G = seq.count("G")
-    T = seq.count("T")
-    bp = A + C + G + T
-
-    if length == bp:
+    if set(seq).issubset(allowed):
+        print("Sequence valid.")
         break
+        
     else:
         seq = input("Sequence invalid (contains NOT only A/C/G/T). Enter new sequence:")
         seq = seq.upper().replace("\n", "").replace(" ", "")
@@ -26,3 +23,4 @@ print("T:", T)
 gc_percent = (G + C) / length * 100
 
 print(f"GC%: {gc_percent:.2f}")
+
