4d9cbf916696e2b6d2e17779f41f4f13994f7f48
PaulJMeyer <pauljonmeyer@web.de>
2026-01-27 12:50:08 +0100

    Update basicDNA.py
    Learned and changed:
    - validation loop using "while True"

--- basicDNA.py
+++ basicDNA.py
@@ -2,16 +2,9 @@ seq = "ATGCAGTAGCACACAGTACAGTGTGACT"
 seq = seq.upper().replace("\n", "").replace(" ", "")
 
 print("Sequenz:", seq)
-length = len(seq)
 
-A = seq.count("A")
-C = seq.count("C")
-G = seq.count("G")
-T = seq.count("T")
-bp = A + C + G + T
+while True:
 
-while length != bp:
-    seq = input("Sequence invalid (contains NOT only A/C/G/T). Enter new sequence:")
     length = len(seq)
     A = seq.count("A")
     C = seq.count("C")
@@ -19,10 +12,17 @@ while length != bp:
     T = seq.count("T")
     bp = A + C + G + T
 
+    if length == bp:
+        break
+    else:
+        seq = input("Sequence invalid (contains NOT only A/C/G/T). Enter new sequence:")
+        seq = seq.upper().replace("\n", "").replace(" ", "")
+
 print("length (in bp):", length)
 print("A:", A)
 print("C:", C)
 print("G:", G)
 print("T:", T)
 gc_percent = (G + C) / length * 100
+
 print(f"GC%: {gc_percent:.2f}")
