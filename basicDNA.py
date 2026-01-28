5d0546b82a56a8b0903d6291f6cfbf6775dec332
PaulJMeyer <pauljonmeyer@web.de>
2026-01-28 11:19:58 +0100

    Version 2.1
    defined length, A, C, G, T again

--- basicDNA.py
+++ basicDNA.py
@@ -10,17 +10,22 @@ while True:
     if set(seq).issubset(allowed):
         print("Sequence valid.")
         break
-        
+    
     else:
         seq = input("Sequence invalid (contains NOT only A/C/G/T). Enter new sequence:")
         seq = seq.upper().replace("\n", "").replace(" ", "")
+
+length = len(seq)
+A = seq.count("A")
+C = seq.count("C")
+G = seq.count("G")
+T = seq.count("T")
 
 print("length (in bp):", length)
 print("A:", A)
 print("C:", C)
 print("G:", G)
 print("T:", T)
+
 gc_percent = (G + C) / length * 100
-
 print(f"GC%: {gc_percent:.2f}")
-
