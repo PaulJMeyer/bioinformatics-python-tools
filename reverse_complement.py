9ac1e88620b4b316f735b40ccb0a7363cc152ebe
PaulJMeyer <pauljonmeyer@web.de>
2026-01-28 12:08:54 +0100

    Version 4
    - added RNA-Transcript
    - added and corrected IUPAC

--- reverse_complement.py
+++ reverse_complement.py
@@ -1,10 +1,11 @@
 seq = "ACTGCTGCAGTCAGTCAGTCAGTCGTCATGCATCTTGCTA"
 seq = seq.upper().replace("\n", "").replace(" ", "")
+seq_type = "coding"
 
-allowed = {"A", "C", "G", "T", "N", "R", "Y" "K", "M", "S", "W"}
-comp = {"A": "T", "T": "A", "G": "C", "C": "G", "N": "N", "R": "Y", "Y": "R", "K": "M", "M": "K", "S": "S", "W": "W"}
+allowed = {"A", "C", "G", "T", "N", "R", "Y", "K", "M", "S", "W", "B", "D", "H", "V", "-"}
+comp = {"A": "T", "T": "A", "G": "C", "C": "G", "N": "N", "R": "Y", "Y": "R", "K": "M", "M": "K", "S": "S", "W": "W", "B": "V", "V": "B", "D": "H", "H": "D", "-": "-"}
 
-print("Sequenz:", seq)
+print("Coding sequence:", seq)
 
 while True:
 
@@ -12,7 +13,7 @@ while True:
         print("Sequence valid.")
         break
     else:
-        seq = input("Sequence invalid (contains NOT only A/C/G/T/N/R/Y/K/M/S/W). Enter new sequence:")
+        seq = input("Sequence invalid (contains NOT only A/C/G/T/N/R/Y/K/M/S/W/B/D/H/V/-). Enter new sequence:")
         seq = seq.upper().replace("\n", "").replace(" ", "")
 
 complement = ""
@@ -21,4 +22,7 @@ for base in seq:
 
 rev_complement = complement[::-1]
 
+rna = seq.replace("T", "U")
+
 print("Reverse Complement:", rev_complement)
+print("RNA transcript:", rna)
