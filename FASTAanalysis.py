0222f11ac1cb6648b56e8165b14319df40b4572f
Paul J. Meyer <pauljonmeyer@web.de>
2026-02-04 16:20:53 +0100

    changed FASTAstring to deal with possible inconsistencies in FASTA formats. Added result to FASTA_analysis as a function

--- FASTAanalysis.py
+++ FASTAanalysis.py
@@ -7,14 +7,46 @@ AAA"""
 
 allowed = {"A", "C", "G", "T", "N", "R", "Y", "K", "M", "S", "W", "B", "D", "H", "V", "-"}
 comp = {"A": "T", "T": "A", "G": "C", "C": "G", "N": "N", "R": "Y", "Y": "R", "K": "M", "M": "K", "S": "S", "W": "W", "B": "V", "V": "B", "D": "H", "H": "D", "-": "-"}
+seq_type = "coding"
 
-def clean_sequence(s):
-    return s.upper().replace("\n", "").replace(" ", "").replace("\t", "")
 
-def is_valid_sequence(seq, allowed):
+def clean_sequence(seq: str) -> str:
+    return seq.upper().replace("\n", "").replace(" ", "").replace("\t", "")
+
+
+def parse_fasta(fasta_text: str) -> list(tuple):
+    lines = fasta_text.splitlines()
+
+    current_header = None
+    current_seq = ""
+    results = []
+
+    for line in lines:
+        line = line.strip()
+        if line.startswith(">"):
+            if current_header != None:
+                results.append((current_header, current_seq))
+            current_header = line[1:].split()[0]
+            current_seq = ""
+        else:
+            clean_line = clean_sequence(line)
+            if clean_line == "":
+                continue
+            if not set(clean_line).issubset(allowed):
+                continue
+            else:
+                current_seq += clean_line
+
+    if current_header != None:
+        results.append((current_header, current_seq))
+    return results
+
+
+def is_valid_sequence(seq: str, allowed: set):
     return set(seq).issubset(allowed)
 
-def reverse_complement(seq, comp):
+
+def reverse_complement(seq: str, comp: dict) -> str:
     complement = ""
     for base in seq:
         complement += comp[base]
@@ -22,7 +54,8 @@ def reverse_complement(seq, comp):
     rev_comp = complement[::-1]
     return rev_comp
 
-def transcribe_to_rna(seq, seq_type, comp):
+
+def transcribe_to_rna(seq: str, seq_type: str, comp: dict) -> str:
     if seq_type == "coding":
         rna_seq = seq.replace("T", "U")
     elif seq_type == "template":
@@ -31,23 +64,3 @@ def transcribe_to_rna(seq, seq_type, comp):
     else:
         rna_seq = "Choose sequence type (coding or template)!"
     return rna_seq
-
-lines = fasta_text.splitlines()
-
-current_header = None
-current_seq = ""
-results = []
-
-for line in lines:
-    if line.startswith(">"):
-        if current_header != None:
-            results.append((current_header, current_seq))
-        current_header = line[1:]
-        current_seq = ""
-    else:
-        current_seq += clean_sequence(line)
-
-if current_header != None:
-    results.append((current_header, current_seq))
-
-print(results)
--- FASTAstring.py
+++ FASTAstring.py
@@ -1,27 +1,87 @@
 fasta_text = """>seq1
-ATGCGTACGTA
+ATGCGTA CGTA
 CGTACGATCG
 >seq2
 TTTGGGCCC
-AAA"""
+AAA"""   # clean FASTA
 
-lines = fasta_text.splitlines()
+fasta_text2 = """
+# this is a comment line
 
-current_header = None
-current_seq = ""
-results = []
+>seq1
+ATGCATGC
+"""   #text/space before first header should be ignored
 
-for line in lines:
-    if line.startswith(">"):
-        if current_header != None:
-            results.append((current_header, current_seq))
-        current_header = line[1:]
-        current_seq = ""
-    else:
-        current_seq += line
+fasta_text3 = """>seq1
+ATGCATGC
 
-if current_header != None:
-    results.append((current_header, current_seq))
+GATTACA
+"""   #empty lines should be ignored
 
 
-print(results)
+fasta_text4 = """>seq1 Homo sapiens BRCA1 gene
+ATGCATGC
+"""   #description of sequence should not be included
+
+fasta_text5 = """>seq1
+>seq2
+ATGC
+"""   #header without sequence -> ignore or include without crash
+
+fasta_text6 = """   >seq1
+ATGCATGC
+"""   #whitespace before >, header should still be recognised
+
+fasta_text7 = """>seq1
+ATGCATGC
+### bookmark ###
+GATTACA
+"""   #lines with invalid symbols should be ignored
+
+fasta_text8 = """>seq1
+ATGC\tAT GC
+GAT\tTACA
+"""   #spaces and tabs should be removed
+
+allowed = {"A", "C", "G", "T", "N", "R", "Y", "K", "M", "S", "W", "B", "D", "H", "V", "-"}
+
+
+def clean_sequence(seq: str) -> str:
+    return seq.upper().replace("\n", "").replace(" ", "").replace("\t", "")
+
+# parse_fasta returning a list:
+def parse_fasta(fasta_text: str) -> list(tuple(str), ):
+    lines = fasta_text.splitlines()
+
+    current_header = None
+    current_seq = ""
+    results = []
+
+    for line in lines:
+        line = line.strip()   #gets rid of whitespaces
+        if line.startswith(">"):
+            if current_header != None:
+                results.append((current_header, current_seq))
+            current_header = line[1:].split()[0]    #removes every word of the header except the first one
+            current_seq = ""
+        else:
+            clean_line = clean_sequence(line)
+            if clean_line == "":                        #ignores empty lines
+                continue
+            if not set(clean_line).issubset(allowed):   #ignores lines that have invalid symbols
+                continue
+            else:
+                current_seq += clean_line
+
+    if current_header != None:
+        results.append((current_header, current_seq))
+    return results
+
+print("1", parse_fasta(fasta_text))
+print("2", parse_fasta(fasta_text2))
+print("3", parse_fasta(fasta_text3))
+print("4", parse_fasta(fasta_text4))
+print("5", parse_fasta(fasta_text5))
+print("6", parse_fasta(fasta_text6))
+print("7", parse_fasta(fasta_text7))
+print("8", parse_fasta(fasta_text8))
